6b8e1d3991ea7c1e245d13d3a135ccde94afae0a
Abdul Momin <abdulmomin114477@gmail.com>
2025-06-28 13:04:23 +0600

    fix: flicker

--- src/components/home-page-clients.tsx
+++ src/components/home-page-clients.tsx
@@ -15,95 +15,97 @@ export const HomePageClients = () => {
 				<div className='text-5xl font-bold leading-14 text-[#333333] mb-[55px]'>
 					We work with the best pepole
 				</div>
-				<Slider
-					{...{
-						dots: false,
-						infinite: true,
-						slidesToShow: 3,
-						slidesToScroll: 1,
-						autoplay: true,
-						speed: 2000,
-						autoplaySpeed: 0,
-						cssEase: 'linear',
-						draggable: false,
-						pauseOnHover: false,
-						rtl: true,
-						variableWidth: true,
-					}}
-				>
-					<div
-						className='h-[128px] w-[239px] relative'
-						style={{ width: 'calc(239px + 72px)' }}
+				<div className='h-[128px] overflow-hidden'>
+					<Slider
+						{...{
+							dots: false,
+							infinite: true,
+							slidesToShow: 3,
+							slidesToScroll: 1,
+							autoplay: true,
+							speed: 2000,
+							autoplaySpeed: 0,
+							cssEase: 'linear',
+							draggable: false,
+							pauseOnHover: false,
+							rtl: true,
+							variableWidth: true,
+						}}
 					>
-						<Image
-							src='/img/client-1.png'
-							alt='Client 1'
-							fill
-							sizes='250px'
-							className='object-contain'
-						></Image>
-					</div>
-					<div
-						className='h-[128px] w-[164px] relative'
-						style={{ width: 'calc(164px + 72px)' }}
-					>
-						<Image
-							src='/img/client-2.png'
-							alt='Client 2'
-							fill
-							sizes='250px'
-							className='object-contain'
-						></Image>
-					</div>
-					<div
-						className='h-[128px] w-[145px] relative'
-						style={{ width: 'calc(145px + 72px)' }}
-					>
-						<Image
-							src='/img/client-3.png'
-							alt='Client 3'
-							fill
-							sizes='250px'
-							className='object-contain'
-						></Image>
-					</div>
-					<div
-						className='h-[128px] w-[129px] relative'
-						style={{ width: 'calc(129px + 72px)' }}
-					>
-						<Image
-							src='/img/client-4.png'
-							alt='Client 4'
-							fill
-							sizes='250px'
-							className='object-contain'
-						></Image>
-					</div>
-					<div
-						className='h-[128px] w-[168px] relative'
-						style={{ width: 'calc(168px + 72px)' }}
-					>
-						<Image
-							src='/img/client-5.png'
-							alt='Client 5'
-							fill
-							sizes='250px'
-							className='object-contain'
-						></Image>
-					</div>
-					<div
-						className='h-[128px] w-[113px] relative'
-						style={{ width: 'calc(113px + 72px)' }}
-					>
-						<Image
-							src='/img/client-6.png'
-							alt='Client 6'
-							fill
-							sizes='250px'
-							className='object-contain'
-						></Image>
-					</div>
-				</Slider>
+						<div
+							className='h-[128px] w-[239px] relative'
+							style={{ width: 'calc(239px + 72px)' }}
+						>
+							<Image
+								src='/img/client-1.png'
+								alt='Client 1'
+								fill
+								sizes='250px'
+								className='object-contain'
+							></Image>
+						</div>
+						<div
+							className='h-[128px] w-[164px] relative'
+							style={{ width: 'calc(164px + 72px)' }}
+						>
+							<Image
+								src='/img/client-2.png'
+								alt='Client 2'
+								fill
+								sizes='250px'
+								className='object-contain'
+							></Image>
+						</div>
+						<div
+							className='h-[128px] w-[145px] relative'
+							style={{ width: 'calc(145px + 72px)' }}
+						>
+							<Image
+								src='/img/client-3.png'
+								alt='Client 3'
+								fill
+								sizes='250px'
+								className='object-contain'
+							></Image>
+						</div>
+						<div
+							className='h-[128px] w-[129px] relative'
+							style={{ width: 'calc(129px + 72px)' }}
+						>
+							<Image
+								src='/img/client-4.png'
+								alt='Client 4'
+								fill
+								sizes='250px'
+								className='object-contain'
+							></Image>
+						</div>
+						<div
+							className='h-[128px] w-[168px] relative'
+							style={{ width: 'calc(168px + 72px)' }}
+						>
+							<Image
+								src='/img/client-5.png'
+								alt='Client 5'
+								fill
+								sizes='250px'
+								className='object-contain'
+							></Image>
+						</div>
+						<div
+							className='h-[128px] w-[113px] relative'
+							style={{ width: 'calc(113px + 72px)' }}
+						>
+							<Image
+								src='/img/client-6.png'
+								alt='Client 6'
+								fill
+								sizes='250px'
+								className='object-contain'
+							></Image>
+						</div>
+					</Slider>
+				</div>
 			</div>
 		</section>
 	);
